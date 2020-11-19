af3f20f3d0483b9c04e599c77cc654bee8e8f1b7
Carlos Casallas <ccasallas@lulobank.com>
2020-11-19 15:41:57 -0500

    Refactor players creation

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,12 +1,7 @@
 package app;
 
 import com.github.javafaker.Faker;
-import domain.Player;
 import domain.Team;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Application {
     public static void main(String[] args) {
@@ -18,18 +13,11 @@ public class Application {
     public void init() {
         Faker faker = new Faker();
 
-        List<Player> players = IntStream.range(0, 11)
-                .mapToObj(index ->  Player.builder()
-                        .name(faker.name().firstName())
-                        .number(index+1)
-                        .build())
-                .collect(Collectors.toList());
-
         Team team = Team.builder()
-                .players(players)
                 .name(faker.team().name())
                 .build();
 
+        team.initFakePlayers();
         team.print();
     }
 }
