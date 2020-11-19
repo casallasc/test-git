97c06132ee195d7b08bba1b3051c999ad6f42a53
Carlos Casallas <73365943+casallasc@users.noreply.github.com>
2020-11-19 15:46:38 -0500

    Refactor players creation (#3)

--- src/main/java/domain/Team.java
+++ src/main/java/domain/Team.java
@@ -1,9 +1,12 @@
 package domain;
 
+import com.github.javafaker.Faker;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Getter
 @Builder
@@ -15,5 +18,15 @@ public class Team {
         System.out.println("Team: " + this.name + "\n");
         this.players
                 .forEach(Player::print);
+    }
+
+    public void initFakePlayers() {
+        Faker faker = new Faker();
+        this.players = IntStream.range(0, 11)
+                .mapToObj(index ->  Player.builder()
+                        .name(faker.name().firstName())
+                        .number(index+1)
+                        .build())
+                .collect(Collectors.toList());
     }
 }
